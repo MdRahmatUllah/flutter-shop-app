9dd92507e3e2dc6e2d5c7c18c29cd752a709c11b
Md Rahmat Ullah <mdrahmatullahmaruf@gmail.com>
2022-02-26 23:26:25 +0600

    data management with provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/models/product.dart';
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail-screen';
   const ProductDetailScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
+    // ProductsProvider productsProvider = new ProductsProvider();
+    // final product = productsProvider.findById(productId);
+    final product = Provider.of<ProductsProvider>(context, listen: false)
+        .findById(productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(product.title),
       ),
       body: Center(
-        child: Text('Product Detail'),
+        child: Text(product.description),
       ),
     );
   }
